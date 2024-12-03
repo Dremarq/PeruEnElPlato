28e8737d5936491c33f24211d3ef58c774c39964
Diego Martin Chavarry Herbay <115725544+dimarYT@users.noreply.github.com>
2024-12-03 14:19:28 -0500

    Update loginCliente.php

--- vista/loginCliente.php
+++ vista/loginCliente.php
@@ -42,7 +42,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <form action="../controlador/CRUDcliente.php" method="POST">
+                    <form action="../controlador/ControllerCliente.php" method="POST">
                         <input type="hidden" name="accion" value="registrar">
                         <div class="mb-3">
                             <label for="nombre" class="form-label">Nombre:</label>
